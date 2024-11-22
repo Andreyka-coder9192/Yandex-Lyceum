8f5b23211c53026f9d7d693437cb5ecb1f4a12f4
Andreyka-coder9192 <andreykalinin2009@gmail.com>
2024-11-22 21:56:31 +0300

    1.4 completed

---
+++ vs-code-Go-projects/sprint-1/task_1.4/file.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+func ExtractLog(inputFileName string, start, end time.Time) ([]string, error) {
+	file, err := os.Open(inputFileName)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка открытия файла")
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var result []string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		dayStr := line[:2]
+		day, err := strconv.Atoi(dayStr)
+		if err != nil {
+			continue
+		}
+		if start.Day() <= day && day <= end.Day() {
+			result = append(result, line)
+			break
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("ошибка сканирования файла")
+	}
+
+	if len(result) == 0 {
+		return nil, fmt.Errorf("ни одна строка не попала в указанный диапазон")
+	}
+
+	return result, nil
+}
